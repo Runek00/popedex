ee37d69c39c57fefa76fa4ba1647c24620c1f7c0
Runek00 <runek00@gmail.com>
2024-01-08 22:51:07 +0100

    So it begins

---
+++ src/main/java/com/example/popedex/PopedexApplication.java
@@ -0,0 +1,13 @@
+package com.example.popedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopedexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/popedex/PopedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.popedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
