502efe372acf8f4f9d83c045ca6ed97c7d44c9e9
Runek00 <runek00@gmail.com>
2024-02-18 21:26:51 +0100

    Some testing is happening

---
+++ src/test/java/com/example/popedex/services/StatueServiceTest.java
@@ -0,0 +1,31 @@
+package com.example.popedex.services;
+
+import com.example.popedex.TestPopedexApplication;
+import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Import({TestPopedexApplication.class})
+@Sql(scripts = "/scripts/statue_test.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
+class StatueServiceTest {
+
+    @Autowired
+    StatueService statueService;
+
+    @Test
+    void countForUser() {
+        assertEquals(statueService.countForUser(1L), 3);
+    }
+
+    @Test
+    void countForUserWrong() {
+        assertThrows(AssertionFailedError.class, () -> assertEquals(statueService.countForUser(1L), 5));
+    }
+}
