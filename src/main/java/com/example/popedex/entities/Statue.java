8ca401c0ddd511026fb9b1b87356d3cbcb3eb281
Runek00 <runek00@gmail.com>
2024-02-29 23:08:49 +0100

    Fixed the db, but I need to make the templates

--- src/main/java/com/example/popedex/entities/Statue.java
+++ src/main/java/com/example/popedex/entities/Statue.java
@@ -1,11 +1,12 @@
 package com.example.popedex.entities;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDate;
 
 @Table(schema = "statue_info")
 public record Statue(@Id Long id, String locationName, LocalDate unveilingDate, Boolean exists,
-                     User addedBy, Boolean active) {
+                     @Column("id") User addedBy, Boolean active) {
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,3 @@
-create schema if not exists statue_info;
-
-create table if not exists statue_info.statue(
-    id bigserial primary key,
-    location_name text not null,
-    unveiling_date date,
-    "exists" bool not null default true,
-    added_by int8,
-    active bool not null default true
-);
-
 create table if not exists users(
     id bigserial,
  	username varchar(50) not null primary key,
@@ -21,12 +10,24 @@ create table if not exists users(
  );
 create unique index if not exists id_idx on users (id);
 
- create table if not exists authorities (
+create table if not exists authorities (
  	username varchar(50) not null,
  	authority varchar(50) not null,
  	constraint fk_authorities_users foreign key(username) references users(username)
- );
- create unique index if not exists ix_auth_username on authorities (username,authority);
+);
+create unique index if not exists ix_auth_username on authorities (username,authority);
+
+create schema if not exists statue_info;
+
+create table if not exists statue_info.statue(
+    id bigserial primary key,
+    location_name text not null,
+    unveiling_date date,
+    "exists" bool not null default true,
+    added_by int8,
+    active bool not null default true,
+    constraint fk_added_by_user foreign key(added_by) references users(id)
+);
 
 create table if not exists statue_info.user_statue(
     id bigserial primary key,
