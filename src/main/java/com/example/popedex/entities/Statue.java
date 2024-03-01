4771b95981e53213ad395ba5c438d3562a396198
Runek00 <runek00@gmail.com>
2024-03-01 20:53:43 +0100

    Fixed the db even more

--- src/main/java/com/example/popedex/entities/Statue.java
+++ src/main/java/com/example/popedex/entities/Statue.java
@@ -1,12 +1,11 @@
 package com.example.popedex.entities;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDate;
 
 @Table(schema = "statue_info")
 public record Statue(@Id Long id, String locationName, LocalDate unveilingDate, Boolean exists,
-                     @Column("id") User addedBy, Boolean active) {
+                     Long addedBy, Boolean active) {
 }
--- src/main/resources/scripts/statue_test.sql
+++ src/main/resources/scripts/statue_test.sql
@@ -1,5 +1,5 @@
 insert into users (id, username, password, visible_name, email, register_time, enabled) values(0, 'test', 'test', 'test', 'test@te.st', now(), true);
 
-insert into statue_info.statue(id, location_name) values(1, 'l1'), (2, 'l2'), (3, 'l3');
+insert into statue_info.statue(id, location_name, added_by) values(1, 'l1', 0), (2, 'l2', null), (3, 'l3', null);
 
 insert into statue_info.user_statue(user_id, statue_id) values(0,1), (0,2), (0,3);
